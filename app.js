e1175a41697940d6c4f843458c4af8a190c03c6e
Carlos Perea <carlosrubenperea@gmail.com>
2022-11-10 10:16:20 -0300

    agrego body con productos

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.resolve(__dirname, 'public'))
 );
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'views/index.html'));
 });
 
 app.get('/register', (req, res) => {
