3c2dac4005d0607103b8fb516f68cc66465ff5e6
Herre3r4 <nahugiuben@gmail.com>
2022-11-30 11:07:40 -0300

    ruteos corregidos en mainControllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 const usersController = {
-    login: (req, res) => {
-        res.sendFile(path.join(__dirname,'../views/users/login.html'));
-      },
+  login: (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/users/login.html"));
+  },
 
-      register:(req,res) => {
-        res.sendFile(path.join(__dirname, '../views/users/register.html'));
-      }
-}
+  register: (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/users/register.html"));
+  },
+};
 
 module.exports = usersController;
