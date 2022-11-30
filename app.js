f86746d523b83c8282348db1abe19055e43cded2
Herre3r4 <nahugiuben@gmail.com>
2022-11-30 09:49:21 -0300

    ruteos corregidos en mainControllers

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path"); // une directorios
 const mainRoutes = require("./routes/mainRoutes.js");
-const userRoutes = require("./routes/userRoutes.js");
-//const PORT = process.env.PORT || 3000;
+//const userRoutes = require("./routes/userRoutes.js");
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, "public")));
 
@@ -11,15 +11,15 @@ app.use(express.static(path.resolve(__dirname, "public")));
 //res.sendFile(path.resolve(__dirname, 'views/index.html'));
 //});
 app.use('/', mainRoutes);
-app.use('/users', userRoutes);
+//app.use('/users', userRoutes);
 
-//app.get('/register', (req, res) => {
-//  res.sendFile(path.resolve(__dirname, 'views/register.html'));
-//});
+app.get('/register', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
 
-//app.get('/login', (req, res) => {
-//  res.sendFile(path.resolve(__dirname, 'views/login.html'));
-//});
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
 
 app.listen(3000, () => {
   console.log('listening on http://localhost:3000');
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 const mainController = {
     index:  (req,res)=>{
-      res.sendFile(path.join(_dirname,'../views/index.html'));  
+      res.sendFile(path.join(__dirname,'../views/index.html'));  
     }
 }
+module.exports = mainController;
